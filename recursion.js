9711f0c0ae019c919f75657ce992872b69a4c876
himanshukumar30 <himanshuk.kumar01@gmail.com>
2023-02-22 18:14:42 -0800

    adding completed recursion exercises

--- recursion.js
+++ recursion.js
@@ -1,5 +1,7 @@
 /** product: calculate the product of an array of numbers. */
 
+const { reverse } = require("dns");
+
 function product(nums, idx = 0) {
   // Base case
   if (idx === nums.length) return 1;
@@ -21,7 +23,7 @@ function longest(words, idx = 0, longestWord = 0) {
 
 /** everyOther: return a string with every other letter. */
 
-function everyOther(str, idx, newStr) {
+function everyOther(str, idx = 0, newStr = "") {
   // Base case
   if (idx >= str.length) return newStr;
 
@@ -37,10 +39,10 @@ function isPalindrome(str, idx = 0) {
   let rightIdx = str.length - idx - 1;
 
   // Base case
-  if (leftIdx >= rightIdx) return True;
+  if (leftIdx >= rightIdx) return true;
 
   // Normal case
-  if (str[leftIdx] !== str[rightIdx]) return False;
+  if (str[leftIdx] !== str[rightIdx]) return false;
 
   return isPalindrome(str, idx + 1);
 }
@@ -52,20 +54,54 @@ function findIndex(arr, val, idx = 0) {
   if (idx === arr.length) return -1;
 
   // Normal case
+  if (arr[idx] === val) return idx;
+
+  return findIndex(arr, val, idx + 1);
 }
 
 /** revString: return a copy of a string, but in reverse. */
 
-function revString(str) {}
+function revString(str, idx = 0, reverseStr = "") {
+  // Base case
+  if (reverseStr.length === str.length) return reverseStr;
+
+  // Normal case
+  reverseStr += str[str.length - 1 - idx];
+
+  return revString(str, idx + 1, reverseStr);
+}
 
 /** gatherStrings: given an object, return an array of all of the string values. */
 
-function gatherStrings(obj) {}
+function gatherStrings(obj, strings = []) {
+  // Base case
+  for (let key in obj) {
+    // Normal case
+    if (typeof obj[key] === "string") strings.push(obj[key]);
+    if (typeof obj[key] === "object") strings.push(...gatherStrings(obj[key]));
+  }
+
+  return strings;
+}
 
 /** binarySearch: given a sorted array of numbers, and a value,
  * return the index of that value (or -1 if val is not present). */
 
-function binarySearch(arr, val) {}
+function binarySearch(arr, val, left = 0, right = arr.length) {
+  // If value not found return -1
+  if (left > right) return -1;
+
+  let middle = Math.floor((left + right) / 2);
+
+  // If value is found
+  if (arr[middle] === val) return middle;
+
+  // If val is on left of middle index
+  if (arr[middle] > val) return binarySearch(arr, val, 0, middle - 1);
+
+  // If val is on right on middle index
+  return binarySearch(arr, val, middle + 1, arr.length);
+}
 
 module.exports = {
   product,
